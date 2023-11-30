25e13712a1c74b84ba41e57e01adff47a8492a36
Aung04 <s10243228@connect.np.edu.sg>
2023-12-01 06:49:12 +0800

    almost finish

--- index.js
+++ index.js
@@ -1,84 +1,51 @@
-const main_video = document.querySelector('.main-video video');
-const main_video_title = document.querySelector('.main-video .title');
-const video_playlist = document.querySelector('.video-playlist .videos');
+let listVideo = document.querySelectorAll('.video-list .vid');
+let mainVideo = document.querySelector('.main-video video');
+let title = document.querySelector('.main-video .title');
 
-let data = [
-    {
-        'id': 'a1',
-        'title': 'Pompeii',
-        'name': 'Pompeii.mp4',
-        'duration': '3:52',
-    },
-    {
-        'id': 'a2',
-        'title': 'Things We Lost In The Fire',
-        'name': 'Things We Lost In The Fire.mp4',
-        'duration': '4:20',
-    },
-    {
-        'id': 'a3',
-        'title': 'Bad Blood',
-        'name': 'Bad Blood.mp4',
-        'duration': '3:33',
-    },
-
-    {
-        'id': 'a4',
-        'title': 'Happier',
-        'name': 'Happier.mp4',
-        'duration': '3:53',
-    },
-    {
-        'id': 'a5',
-        'title': 'Laura Palmer',
-        'name': 'Laura Palmer.mp4',
-        'duration': '3:20',
-    },
-    {
-        'id': 'a6',
-        'title': 'Send Them Off!',
-        'name': 'Send Them Off!.mp4',
-        'duration': '3:44',
-    },
-    {
-        'id': 'a7',
-        'title': 'Flaws',
-        'name': 'Flaws.mp4',
-        'duration': '3:40',
-    },
-  
-];
-
-data.forEach((video, i) => {
-    let video_element = `
-                <div class="video" data-id="${video.id}">
-                    <img src="images/play.png" alt="">
-                    <p>${i + 1 > 9 ? i + 1 : '0' + (i + 1)}. </p>
-                    <h3 class="title">${video.title}</h3>
-                    <p class="time">${video.duration}</p>
-                </div>
-    `;
-    video_playlist.innerHTML += video_element;
-})
-
-let videos = document.querySelectorAll('.video');
-videos[0].classList.add('active');
-videos[0].querySelector('img').src = 'images/pause.png';
-
-videos.forEach(selected_video => {
-    selected_video.onclick = () => {
-
-        for (all_videos of videos) {
-            all_videos.classList.remove('active');
-            all_videos.querySelector('img').src = 'images/play.png';
-
+listVideo.forEach(video =>{
+    video.onclick = () =>{
+        listVideo.forEach(vid => vid.classList.remove('active'));
+        video.classList.add('active');
+        if(video.classList.contains('active')){
+            let src =video.children[0].getAttribute('src');
+            mainVideo.src =src;
+            let text =video.children[1].innerHTML;
+            title.innerHTML =text;
         }
+    };
+ });
+ const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
 
-        selected_video.classList.add('active');
-        selected_video.querySelector('img').src = 'images/pause.png';
-
-        let match_video = data.find(video => video.id == selected_video.dataset.id);
-        main_video.src = 'videos/' + match_video.name;
-        main_video_title.innerHTML = match_video.title;
-    }
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
 });
+
+var swiper = new Swiper(".slider-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide:'true',
+    fade:'true',
+    grabCursor:'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets:true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        520:{
+            slidesPerView:2,
+        },
+        950:{
+            slidesPerView:3,
+        },
+    }
+  });
